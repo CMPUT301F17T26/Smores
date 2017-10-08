4bdb6b27f40ba1e6cae0503324feb8a9a2babe0d
Rohan <rohan@ualberta.ca>
2017-10-08 17:16:37 -0600

    Added date picker fragment

--- app/src/main/java/cmput301f17t26/smores/all_activities/HabitDetailsActivity.java
+++ app/src/main/java/cmput301f17t26/smores/all_activities/HabitDetailsActivity.java
@@ -1,15 +1,26 @@
 package cmput301f17t26.smores.all_activities;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Toast;
 
 import cmput301f17t26.smores.R;
 
 public class HabitDetailsActivity extends AppCompatActivity {
+
+    private Button mDateSelect;
+    private int mYear;
+    private int mMonth;
+    private int mDay;
+    private static final int DIALOG_ID = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +29,29 @@ public class HabitDetailsActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mDateSelect = (Button) findViewById(R.id.Habit_dateBtn);
+        mDateSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog(DIALOG_ID);
+            }
+        });
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        if (id == DIALOG_ID) {
+            return new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                    mYear = year;
+                    mMonth = month;
+                    mDay = day;
+                }
+            }, mYear, mMonth, mDay);
+        }
+        return null;
     }
 
 }
