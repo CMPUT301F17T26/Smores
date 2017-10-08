38eea383325454a56d499b21478c26d9004e5b11
Rohan <rohan@ualberta.ca>
2017-10-08 17:23:30 -0600

    Updated date picker code

--- app/src/main/java/cmput301f17t26/smores/all_fragments/AddDialogFragment.java
+++ app/src/main/java/cmput301f17t26/smores/all_fragments/AddDialogFragment.java
@@ -2,9 +2,7 @@ package cmput301f17t26.smores.all_fragments;
 
 import android.os.Bundle;
 
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.DialogFragment;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
