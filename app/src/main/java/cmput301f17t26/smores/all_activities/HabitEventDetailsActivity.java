8a7c164fa511341c6994c35298de651032bf9f22
Chris <cfrancis@ualberta.ca>
2017-11-09 14:02:05 -0700

    added habbit event camera

--- app/src/main/java/cmput301f17t26/smores/all_activities/HabitEventDetailsActivity.java
+++ app/src/main/java/cmput301f17t26/smores/all_activities/HabitEventDetailsActivity.java
@@ -1,7 +1,12 @@
 package cmput301f17t26.smores.all_activities;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.media.Image;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -12,11 +17,14 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import cmput301f17t26.smores.R;
 
 public class HabitEventDetailsActivity extends AppCompatActivity {
+    public static final int REQUEST_CODE = 0;
+    public static final int CAMERA_REQUEST = 1;
     private Spinner mHabitType;
     private EditText mComment;
     private TextView mDateCompleted;
@@ -42,7 +50,43 @@ public class HabitEventDetailsActivity extends AppCompatActivity {
         mImageView = (ImageView) findViewById((R.id.Event_hImage));
         mSave = (ImageButton) findViewById(R.id.Event_hSave);
         mDelete = (ImageButton) findViewById(R.id.Event_hDelete);
+
+        mImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                    invokeCamera();
+                } else {
+                    String[] permissionRequested = {Manifest.permission.CAMERA};
+                    requestPermissions(permissionRequested, REQUEST_CODE);
+                }
+            }
+        });
     }
 
-    
+    public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                invokeCamera();
+            } else {
+                Toast.makeText(this, "Unable to request camera", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    public void invokeCamera() {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+        }
+    }
+
 }
