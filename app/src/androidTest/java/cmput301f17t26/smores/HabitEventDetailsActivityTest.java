6dd745297a10042897d93b514b53546dbed018fa
Farhad Makiabady <makiabad@ualberta.ca>
2017-11-11 18:51:48 -0700

    Set up intent testing files

---
+++ app/src/androidTest/java/cmput301f17t26/smores/HabitEventDetailsActivityTest.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2017 Team 26, CMPUT 301, University of Alberta - All Rights Reserved.
+ * You may use, distribute, or modify this code under terms and conditions of the Code of Student Behavior at University of Alberta.
+ * You can find a copy of the license in this project. Otherwise please contact rohan@ualberta.ca
+ */
+
+package cmput301f17t26.smores;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.robotium.solo.Solo;
+
+import cmput301f17t26.smores.all_activities.HabitEventDetailsActivity;
+
+/**
+ * Created by farhadmakiabady on 2017-11-11.
+ */
+
+public class HabitEventDetailsActivityTest extends ActivityInstrumentationTestCase2<HabitEventDetailsActivity> {
+    private Solo solo;
+}
---
+++ app/src/androidTest/java/cmput301f17t26/smores/MainActivityTest.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2017 Team 26, CMPUT 301, University of Alberta - All Rights Reserved.
+ * You may use, distribute, or modify this code under terms and conditions of the Code of Student Behavior at University of Alberta.
+ * You can find a copy of the license in this project. Otherwise please contact rohan@ualberta.ca
+ */
+
+package cmput301f17t26.smores;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import cmput301f17t26.smores.all_activities.MainActivity;
+
+import com.robotium.solo.Solo;
+
+/**
+ * Created by farhadmakiabady on 2017-11-11.
+ */
+
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+    private Solo solo;
+}
