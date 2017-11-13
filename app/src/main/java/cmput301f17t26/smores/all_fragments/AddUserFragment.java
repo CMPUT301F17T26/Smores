9b5a7aed52eb7e884421ea58b960c231e51b508a
Rohan <rohan@ualberta.ca>
2017-11-13 11:11:29 -0700

    Added some docs

--- app/src/main/java/cmput301f17t26/smores/all_fragments/AddUserFragment.java
+++ app/src/main/java/cmput301f17t26/smores/all_fragments/AddUserFragment.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2017 Team 26, CMPUT 301, University of Alberta - All Rights Reserved.
+ * You may use, distribute, or modify this code under terms and conditions of the Code of Student Behavior at University of Alberta.
+ * You can find a copy of the license in this project. Otherwise please contact rohan@ualberta.ca
+ */
+
 package cmput301f17t26.smores.all_fragments;
 
 import android.os.Bundle;
