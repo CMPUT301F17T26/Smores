a61c8d9dd46a3de9723947c317ee42750fc1476c
Rohan <rohan@ualberta.ca>
2017-11-23 13:58:09 -0700

    Small if statement thats for an edge case for notifications

--- app/src/main/java/cmput301f17t26/smores/utils/Notification_reciever.java
+++ app/src/main/java/cmput301f17t26/smores/utils/Notification_reciever.java
@@ -66,10 +66,9 @@ public class Notification_reciever extends BroadcastReceiver {
         calendar.set(Calendar.HOUR_OF_DAY, 8);
         calendar.set(Calendar.MINUTE, 30);
 
-        //if (calendar.getTimeInMillis() < System.currentTimeMillis()) {
-            //calendar.add(Calendar.DATE, 1);
-        //}
-
+        if (calendar.getTimeInMillis() < System.currentTimeMillis()) {
+            calendar.add(Calendar.DATE, 1);
+        }
 
         Intent intent = new Intent(context, Notification_reciever.class);
         intent.putExtra("NOTIFICATION_EXTRA", 1);
