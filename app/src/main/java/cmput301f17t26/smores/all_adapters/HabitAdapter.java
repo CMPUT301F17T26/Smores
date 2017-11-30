695ae4324dbf9d657de1427bae78bcc2eaa9a5b9
AkashPatelUAlberta <akpatel@ualberta.ca>
2017-11-30 11:34:09 -0700

    Modified code to fit new images

--- app/src/main/java/cmput301f17t26/smores/all_adapters/HabitAdapter.java
+++ app/src/main/java/cmput301f17t26/smores/all_adapters/HabitAdapter.java
@@ -97,207 +97,207 @@ public class HabitAdapter extends RecyclerView.Adapter<HabitAdapter.ViewHolder> 
 
         switch (progress) {
             case 0:
-                holder.mProgress.setImageResource(R.drawable.progress000); break;
+                holder.mProgress.setImageResource(R.drawable.bar000); break;
             case 1:
-                holder.mProgress.setImageResource(R.drawable.progress001); break;
+                holder.mProgress.setImageResource(R.drawable.bar001); break;
             case 2:
-                holder.mProgress.setImageResource(R.drawable.progress002); break;
+                holder.mProgress.setImageResource(R.drawable.bar002); break;
             case 3:
-                holder.mProgress.setImageResource(R.drawable.progress003); break;
+                holder.mProgress.setImageResource(R.drawable.bar003); break;
             case 4:
-                holder.mProgress.setImageResource(R.drawable.progress004); break;
+                holder.mProgress.setImageResource(R.drawable.bar004); break;
             case 5:
-                holder.mProgress.setImageResource(R.drawable.progress005); break;
+                holder.mProgress.setImageResource(R.drawable.bar005); break;
             case 6:
-                holder.mProgress.setImageResource(R.drawable.progress006); break;
+                holder.mProgress.setImageResource(R.drawable.bar006); break;
             case 7:
-                holder.mProgress.setImageResource(R.drawable.progress007); break;
+                holder.mProgress.setImageResource(R.drawable.bar007); break;
             case 8:
-                holder.mProgress.setImageResource(R.drawable.progress008); break;
+                holder.mProgress.setImageResource(R.drawable.bar008); break;
             case 9:
-                holder.mProgress.setImageResource(R.drawable.progress009); break;
+                holder.mProgress.setImageResource(R.drawable.bar009); break;
             case 10:
-                holder.mProgress.setImageResource(R.drawable.progress010); break;
+                holder.mProgress.setImageResource(R.drawable.bar010); break;
             case 11:
-                holder.mProgress.setImageResource(R.drawable.progress011); break;
+                holder.mProgress.setImageResource(R.drawable.bar011); break;
             case 12:
-                holder.mProgress.setImageResource(R.drawable.progress012); break;
+                holder.mProgress.setImageResource(R.drawable.bar012); break;
             case 13:
-                holder.mProgress.setImageResource(R.drawable.progress013); break;
+                holder.mProgress.setImageResource(R.drawable.bar013); break;
             case 14:
-                holder.mProgress.setImageResource(R.drawable.progress014); break;
+                holder.mProgress.setImageResource(R.drawable.bar014); break;
             case 15:
-                holder.mProgress.setImageResource(R.drawable.progress015); break;
+                holder.mProgress.setImageResource(R.drawable.bar015); break;
             case 16:
-                holder.mProgress.setImageResource(R.drawable.progress016); break;
+                holder.mProgress.setImageResource(R.drawable.bar016); break;
             case 17:
-                holder.mProgress.setImageResource(R.drawable.progress017); break;
+                holder.mProgress.setImageResource(R.drawable.bar017); break;
             case 18:
-                holder.mProgress.setImageResource(R.drawable.progress018); break;
+                holder.mProgress.setImageResource(R.drawable.bar018); break;
             case 19:
-                holder.mProgress.setImageResource(R.drawable.progress019); break;
+                holder.mProgress.setImageResource(R.drawable.bar019); break;
             case 20:
-                holder.mProgress.setImageResource(R.drawable.progress020); break;
+                holder.mProgress.setImageResource(R.drawable.bar020); break;
             case 21:
-                holder.mProgress.setImageResource(R.drawable.progress021); break;
+                holder.mProgress.setImageResource(R.drawable.bar021); break;
             case 22:
-                holder.mProgress.setImageResource(R.drawable.progress022); break;
+                holder.mProgress.setImageResource(R.drawable.bar022); break;
             case 23:
-                holder.mProgress.setImageResource(R.drawable.progress023); break;
+                holder.mProgress.setImageResource(R.drawable.bar023); break;
             case 24:
-                holder.mProgress.setImageResource(R.drawable.progress024); break;
+                holder.mProgress.setImageResource(R.drawable.bar024); break;
             case 25:
-                holder.mProgress.setImageResource(R.drawable.progress025); break;
+                holder.mProgress.setImageResource(R.drawable.bar025); break;
             case 26:
-                holder.mProgress.setImageResource(R.drawable.progress026); break;
+                holder.mProgress.setImageResource(R.drawable.bar026); break;
             case 27:
-                holder.mProgress.setImageResource(R.drawable.progress027); break;
+                holder.mProgress.setImageResource(R.drawable.bar027); break;
             case 28:
-                holder.mProgress.setImageResource(R.drawable.progress028); break;
+                holder.mProgress.setImageResource(R.drawable.bar028); break;
             case 29:
-                holder.mProgress.setImageResource(R.drawable.progress029); break;
+                holder.mProgress.setImageResource(R.drawable.bar029); break;
             case 30:
-                holder.mProgress.setImageResource(R.drawable.progress030); break;
+                holder.mProgress.setImageResource(R.drawable.bar030); break;
             case 31:
-                holder.mProgress.setImageResource(R.drawable.progress031); break;
+                holder.mProgress.setImageResource(R.drawable.bar031); break;
             case 32:
-                holder.mProgress.setImageResource(R.drawable.progress032); break;
+                holder.mProgress.setImageResource(R.drawable.bar032); break;
             case 33:
-                holder.mProgress.setImageResource(R.drawable.progress033); break;
+                holder.mProgress.setImageResource(R.drawable.bar033); break;
             case 34:
-                holder.mProgress.setImageResource(R.drawable.progress034); break;
+                holder.mProgress.setImageResource(R.drawable.bar034); break;
             case 35:
-                holder.mProgress.setImageResource(R.drawable.progress035); break;
+                holder.mProgress.setImageResource(R.drawable.bar035); break;
             case 36:
-                holder.mProgress.setImageResource(R.drawable.progress036); break;
+                holder.mProgress.setImageResource(R.drawable.bar036); break;
             case 37:
-                holder.mProgress.setImageResource(R.drawable.progress037); break;
+                holder.mProgress.setImageResource(R.drawable.bar037); break;
             case 38:
-                holder.mProgress.setImageResource(R.drawable.progress038); break;
+                holder.mProgress.setImageResource(R.drawable.bar038); break;
             case 39:
-                holder.mProgress.setImageResource(R.drawable.progress039); break;
+                holder.mProgress.setImageResource(R.drawable.bar039); break;
             case 40:
-                holder.mProgress.setImageResource(R.drawable.progress040); break;
+                holder.mProgress.setImageResource(R.drawable.bar040); break;
             case 41:
-                holder.mProgress.setImageResource(R.drawable.progress041); break;
+                holder.mProgress.setImageResource(R.drawable.bar041); break;
             case 42:
-                holder.mProgress.setImageResource(R.drawable.progress042); break;
+                holder.mProgress.setImageResource(R.drawable.bar042); break;
             case 43:
-                holder.mProgress.setImageResource(R.drawable.progress043); break;
+                holder.mProgress.setImageResource(R.drawable.bar043); break;
             case 44:
-                holder.mProgress.setImageResource(R.drawable.progress044); break;
+                holder.mProgress.setImageResource(R.drawable.bar044); break;
             case 45:
-                holder.mProgress.setImageResource(R.drawable.progress045); break;
+                holder.mProgress.setImageResource(R.drawable.bar045); break;
             case 46:
-                holder.mProgress.setImageResource(R.drawable.progress046); break;
+                holder.mProgress.setImageResource(R.drawable.bar046); break;
             case 47:
-                holder.mProgress.setImageResource(R.drawable.progress047); break;
+                holder.mProgress.setImageResource(R.drawable.bar047); break;
             case 48:
-                holder.mProgress.setImageResource(R.drawable.progress048); break;
+                holder.mProgress.setImageResource(R.drawable.bar048); break;
             case 49:
-                holder.mProgress.setImageResource(R.drawable.progress049); break;
+                holder.mProgress.setImageResource(R.drawable.bar049); break;
             case 50:
-                holder.mProgress.setImageResource(R.drawable.progress050); break;
+                holder.mProgress.setImageResource(R.drawable.bar050); break;
             case 51:
-                holder.mProgress.setImageResource(R.drawable.progress051); break;
+                holder.mProgress.setImageResource(R.drawable.bar051); break;
             case 52:
-                holder.mProgress.setImageResource(R.drawable.progress052); break;
+                holder.mProgress.setImageResource(R.drawable.bar052); break;
             case 53:
-                holder.mProgress.setImageResource(R.drawable.progress053); break;
+                holder.mProgress.setImageResource(R.drawable.bar053); break;
             case 54:
-                holder.mProgress.setImageResource(R.drawable.progress054); break;
+                holder.mProgress.setImageResource(R.drawable.bar054); break;
             case 55:
-                holder.mProgress.setImageResource(R.drawable.progress055); break;
+                holder.mProgress.setImageResource(R.drawable.bar055); break;
             case 56:
-                holder.mProgress.setImageResource(R.drawable.progress056); break;
+                holder.mProgress.setImageResource(R.drawable.bar056); break;
             case 57:
-                holder.mProgress.setImageResource(R.drawable.progress057); break;
+                holder.mProgress.setImageResource(R.drawable.bar057); break;
             case 58:
-                holder.mProgress.setImageResource(R.drawable.progress058); break;
+                holder.mProgress.setImageResource(R.drawable.bar058); break;
             case 59:
-                holder.mProgress.setImageResource(R.drawable.progress059); break;
+                holder.mProgress.setImageResource(R.drawable.bar059); break;
             case 60:
-                holder.mProgress.setImageResource(R.drawable.progress060); break;
+                holder.mProgress.setImageResource(R.drawable.bar060); break;
             case 61:
-                holder.mProgress.setImageResource(R.drawable.progress061); break;
+                holder.mProgress.setImageResource(R.drawable.bar061); break;
             case 62:
-                holder.mProgress.setImageResource(R.drawable.progress062); break;
+                holder.mProgress.setImageResource(R.drawable.bar062); break;
             case 63:
-                holder.mProgress.setImageResource(R.drawable.progress063); break;
+                holder.mProgress.setImageResource(R.drawable.bar063); break;
             case 64:
-                holder.mProgress.setImageResource(R.drawable.progress064); break;
+                holder.mProgress.setImageResource(R.drawable.bar064); break;
             case 65:
-                holder.mProgress.setImageResource(R.drawable.progress065); break;
+                holder.mProgress.setImageResource(R.drawable.bar065); break;
             case 66:
-                holder.mProgress.setImageResource(R.drawable.progress066); break;
+                holder.mProgress.setImageResource(R.drawable.bar066); break;
             case 67:
-                holder.mProgress.setImageResource(R.drawable.progress067); break;
+                holder.mProgress.setImageResource(R.drawable.bar067); break;
             case 68:
-                holder.mProgress.setImageResource(R.drawable.progress068); break;
+                holder.mProgress.setImageResource(R.drawable.bar068); break;
             case 69:
-                holder.mProgress.setImageResource(R.drawable.progress069); break;
+                holder.mProgress.setImageResource(R.drawable.bar069); break;
             case 70:
-                holder.mProgress.setImageResource(R.drawable.progress070); break;
+                holder.mProgress.setImageResource(R.drawable.bar070); break;
             case 71:
-                holder.mProgress.setImageResource(R.drawable.progress071); break;
+                holder.mProgress.setImageResource(R.drawable.bar071); break;
             case 72:
-                holder.mProgress.setImageResource(R.drawable.progress072); break;
+                holder.mProgress.setImageResource(R.drawable.bar072); break;
             case 73:
-                holder.mProgress.setImageResource(R.drawable.progress073); break;
+                holder.mProgress.setImageResource(R.drawable.bar073); break;
             case 74:
-                holder.mProgress.setImageResource(R.drawable.progress074); break;
+                holder.mProgress.setImageResource(R.drawable.bar074); break;
             case 75:
-                holder.mProgress.setImageResource(R.drawable.progress075); break;
+                holder.mProgress.setImageResource(R.drawable.bar075); break;
             case 76:
-                holder.mProgress.setImageResource(R.drawable.progress076); break;
+                holder.mProgress.setImageResource(R.drawable.bar076); break;
             case 77:
-                holder.mProgress.setImageResource(R.drawable.progress077); break;
+                holder.mProgress.setImageResource(R.drawable.bar077); break;
             case 78:
-                holder.mProgress.setImageResource(R.drawable.progress078); break;
+                holder.mProgress.setImageResource(R.drawable.bar078); break;
             case 79:
-                holder.mProgress.setImageResource(R.drawable.progress079); break;
+                holder.mProgress.setImageResource(R.drawable.bar079); break;
             case 80:
-                holder.mProgress.setImageResource(R.drawable.progress080); break;
+                holder.mProgress.setImageResource(R.drawable.bar080); break;
             case 81:
-                holder.mProgress.setImageResource(R.drawable.progress081); break;
+                holder.mProgress.setImageResource(R.drawable.bar081); break;
             case 82:
-                holder.mProgress.setImageResource(R.drawable.progress082); break;
+                holder.mProgress.setImageResource(R.drawable.bar082); break;
             case 83:
-                holder.mProgress.setImageResource(R.drawable.progress083); break;
+                holder.mProgress.setImageResource(R.drawable.bar083); break;
             case 84:
-                holder.mProgress.setImageResource(R.drawable.progress084); break;
+                holder.mProgress.setImageResource(R.drawable.bar084); break;
             case 85:
-                holder.mProgress.setImageResource(R.drawable.progress085); break;
+                holder.mProgress.setImageResource(R.drawable.bar085); break;
             case 86:
-                holder.mProgress.setImageResource(R.drawable.progress086); break;
+                holder.mProgress.setImageResource(R.drawable.bar086); break;
             case 87:
-                holder.mProgress.setImageResource(R.drawable.progress087); break;
+                holder.mProgress.setImageResource(R.drawable.bar087); break;
             case 88:
-                holder.mProgress.setImageResource(R.drawable.progress088); break;
+                holder.mProgress.setImageResource(R.drawable.bar088); break;
             case 89:
-                holder.mProgress.setImageResource(R.drawable.progress089); break;
+                holder.mProgress.setImageResource(R.drawable.bar089); break;
             case 90:
-                holder.mProgress.setImageResource(R.drawable.progress090); break;
+                holder.mProgress.setImageResource(R.drawable.bar090); break;
             case 91:
-                holder.mProgress.setImageResource(R.drawable.progress091); break;
+                holder.mProgress.setImageResource(R.drawable.bar091); break;
             case 92:
-                holder.mProgress.setImageResource(R.drawable.progress092); break;
+                holder.mProgress.setImageResource(R.drawable.bar092); break;
             case 93:
-                holder.mProgress.setImageResource(R.drawable.progress093); break;
+                holder.mProgress.setImageResource(R.drawable.bar093); break;
             case 94:
-                holder.mProgress.setImageResource(R.drawable.progress094); break;
+                holder.mProgress.setImageResource(R.drawable.bar094); break;
             case 95:
-                holder.mProgress.setImageResource(R.drawable.progress095); break;
+                holder.mProgress.setImageResource(R.drawable.bar095); break;
             case 96:
-                holder.mProgress.setImageResource(R.drawable.progress096); break;
+                holder.mProgress.setImageResource(R.drawable.bar096); break;
             case 97:
-                holder.mProgress.setImageResource(R.drawable.progress097); break;
+                holder.mProgress.setImageResource(R.drawable.bar097); break;
             case 98:
-                holder.mProgress.setImageResource(R.drawable.progress098); break;
+                holder.mProgress.setImageResource(R.drawable.bar098); break;
             case 99:
-                holder.mProgress.setImageResource(R.drawable.progress099); break;
+                holder.mProgress.setImageResource(R.drawable.bar099); break;
             case 100:
-                holder.mProgress.setImageResource(R.drawable.progress100); break;
+                holder.mProgress.setImageResource(R.drawable.bar100); break;
             default:
                 holder.mProgress.setImageResource(R.drawable.errorbar); break;
         }
